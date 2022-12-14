5966708bf861d530da33c4deef1ef097e06076a4
Magda-Kilonzo <magdalenekilonzo4@gmail.com>
2022-12-14 10:04:46 -0800

    islower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -10,11 +10,11 @@ int main(void)
 	int r;
 
 	r = _islower('H);
-	putchar(r + '0');
+	_putchar(r + '0');
 	r = _islower('o');
-	putchar(r + '0');
+	_putchar(r + '0');
 	r = islower(108);
-	putchar(r + '0');
+	_putchar(r + '0');
 	putchar('\n');
 	return (0);
 }
