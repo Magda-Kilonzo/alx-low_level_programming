946041deaaaae55b73a1ed9c188208c3f4959204
Magda-Kilonzo <magdalenekilonzo4@gmail.com>
2022-12-14 12:59:13 -0800

    There is no such thing as absolute value in this world

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of a number
+ * @i: Value to test
+ *
+ * Return: absolute value of i
+ */
+int _abs(int i)
+{
+	return (i < 0 ? -i : i);
+}
