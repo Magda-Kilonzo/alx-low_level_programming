f9e8ba5208057de0de4d4b050963774fc9e8b876
Magda-Kilonzo <magdalenekilonzo4@gmail.com>
2022-12-22 07:54:41 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - C function that copies a string including the
+ * terminating null byte, using at mst an inputted number of bytes
+ * When the length of source is less than maximum byte number,
+ * the remainder of the destination string if filled will null bytes
+ * Works identically to the standard library function
+ * @dest: buffer storing the string copy
+ * @src: source string
+ * @n: max number of byte coied
+ * Returns: returns
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
